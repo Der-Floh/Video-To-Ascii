03bf643fcb67717713a6a8de9622ffa34e653cce
Der-Floh <derr.floh@gmail.com>
2025-06-19 14:18:02 +0200

    fix

--- Package/chocolatey/videotoascii/tools/chocolateyInstall.ps1
+++ Package/chocolatey/videotoascii/tools/chocolateyInstall.ps1
@@ -1,12 +1,20 @@
-$packageName  = 'videotoascii'
-$version      = '{{VERSION}}'
-$checksum     = '{{SHA256}}'
-$url          = "https://github.com/Der-Floh/Video-To-Ascii/releases/download/v$version/VideoToAscii-v$version.msi"
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
-Install-ChocolateyPackage `
-  -PackageName $packageName `
-  -FileType 'msi' `
-  -SilentArgs '/quiet' `
-  -Url $url `
-  -Checksum $checksum `
-  -ChecksumType 'sha256'
+$packageName = 'videotoascii'
+$version = '{{VERSION}}'
+$checksum = '{{SHA256}}'
+$url = "https://github.com/Der-Floh/Video-To-Ascii/releases/download/v$version/VideoToAscii-v$version.msi"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  url            = $url
+  softwareName   = $packageName
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
