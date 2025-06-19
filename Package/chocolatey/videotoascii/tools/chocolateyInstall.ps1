68491cce3c517bf5e438c35493189f19a0b083b2
Der-Floh <derr.floh@gmail.com>
2025-06-19 13:25:52 +0200

    fix

--- Package/chocolatey/videotoascii/tools/chocolateyInstall.ps1
+++ Package/chocolatey/videotoascii/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName  = 'videotoascii'
 $version      = '{{VERSION}}'
-$url          = "https://github.com/Der-Floh/Video-To-Ascii/releases/download/v$version/VideoToAscii-v$version.msi"
 $checksum     = '{{SHA256}}'
+$url          = "https://github.com/Der-Floh/Video-To-Ascii/releases/download/v$version/VideoToAscii-v$version.msi"
 
 Install-ChocolateyPackage `
   -PackageName $packageName `
